96bfbcb9b3f3c064d39dc78816332925e71a81b2
Alexander Lyotov <sippie84@gmail.com>
2021-10-18 13:59:44 +0300

    debuged new config and installed wemake linter

--- parser/__main__.py
+++ parser/__main__.py
@@ -1,20 +1,28 @@
 import logging
 from parser.book_site_parser import SiteParser
+from parser.config import load_config
 from parser.save_dataclass_list_into_csv import save_list_of_dataclass_objs_to_csv
-from parser.config import (resource_url, robots_txt, num_items_to_parse, max_id_num_to_process,
-                           first_item_id)
-
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(filename)s | %(levelname)s: %(message)s')
 
 
 if __name__ == '__main__':
-    site_parser = SiteParser(url=resource_url,
-                             robots_txt=robots_txt,
-                             books_number=num_items_to_parse,
-                             max_ids_to_process=max_id_num_to_process,
-                             first_book_id=first_item_id)
+    config = load_config()
+    parse_result = urlparse(config.site.format(config.first_id))
+    scheme = parse_result.scheme
+    netloc = parse_result.netloc
+
+    robots_txt = f'{scheme}://{netloc}/robots.txt'
+
+    site_parser = SiteParser(
+                  url=config.site,
+                  robots_txt=robots_txt,
+                  books_number=config.parse_number,
+                  max_ids_to_process=config.max_ids,
+                  first_book_id=config.first_id,
+                  )
     logger.info('Start.')
     resulting_books = site_parser.parse()
     logger.debug('Resulting book list now is about to be saved to csv!')
--- parser/config.py
+++ parser/config.py
@@ -1,7 +1,19 @@
 import os
+from dataclasses import dataclass
 
-resource_url = os.environ['SITE_ITEMS_PATTERN']
-robots_txt = os.environ['ROBOTS_TXT']
-num_items_to_parse = int(os.environ['NUMBER_ITEMS_TO_PARSE'])
-max_id_num_to_process = int(os.environ['MAX_IDS_TO_PROCESS'])
-first_item_id = int(os.environ['FIRST_ITEM_ID'])
+
+@dataclass
+class AppConfig:
+    site: str
+    parse_number: int
+    max_ids: int
+    first_id: int
+
+
+def load_config():
+    return AppConfig(
+        site=os.environ['SITE_ITEMS_PATTERN'],
+        parse_number=int(os.environ['NUMBER_ITEMS_TO_PARSE']),
+        max_ids=int(os.environ['MAX_IDS_TO_PROCESS']),
+        first_id=int(os.environ['FIRST_ITEM_ID']),
+    )
